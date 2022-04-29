60c5691e18a1157265ec0a56115e32263770aba8
umang balsara <umangbalsara@knovator.in>
2022-04-29 12:42:53 +0530

    Body Scroll lock on Modal

--- hooks/useModal.jsx
+++ hooks/useModal.jsx
@@ -1,17 +1,22 @@
 import { useState, useCallback } from "react";
+import { useScrollLock } from "@mantine/hooks";
 
 const useModal = () => {
   const [open, setOpen] = useState(false);
 
+  const [scrollLocked, setScrollLocked] = useScrollLock();
+
   const openModal = useCallback(() => {
-    document.body.classList.add("prevent_scroll");
+    // document.body.classList.add("prevent_scroll");
+    setScrollLocked(true);
     setOpen(true);
-  }, [setOpen]);
+  }, [setOpen, setScrollLocked]);
 
   const closeModal = useCallback(() => {
-    document.body.classList.remove("prevent_scroll");
+    // document.body.classList.remove("prevent_scroll");
+    setScrollLocked(false);
     setOpen(false);
-  }, [setOpen]);
+  }, [setOpen, setScrollLocked]);
 
   return {
     open,
